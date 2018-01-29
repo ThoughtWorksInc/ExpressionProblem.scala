df698d1444a2baa8acafb483f52cac4f073b71de
Yang Bo <pop.atry@gmail.com>
2018-01-29 16:31:04 +0800

    example-test-wip

--- Expressions/src/main/scala/com/thoughtworks/expressions/tree/StructuralTrees.scala
+++ Expressions/src/main/scala/com/thoughtworks/expressions/tree/StructuralTrees.scala
@@ -9,6 +9,45 @@ import scala.collection.mutable.ArrayBuffer
   */
 trait StructuralTrees extends Trees {
 
+  // TODO: enable sbt-example for this plug-in.
+  /** The key type for [[kernelCache]], which structurally performs [[hashCode]] and [[equals]] on [[term]].
+    *
+    * @example Given two [[Term]]s that contains the same operations but different [[Type#Identifier]],
+    *
+    *          {{{
+    *          val a = float.Identifier()
+    *          val b = float.Identifier()
+    *          val x = float.Identifier()
+    *          val y = float.Identifier()
+    *          val aPlusB = a + b
+    *          val xPlusY = x + y
+    *          }}}
+    *
+    *          then they should not equal to each other,
+    *
+    *          {{{
+    *          aPlusB shouldNot be(xPlusY)
+    *          }}}
+    *
+    *          However, when wrappingThem as [[StructuralKey]],
+    *
+    *          {{{
+    *          val structuralAPlusB = StructuralKey(aPlusB)
+    *          val structuralXPlusY = StructuralKey(xPlusY)
+    *          }}}
+    *
+    *          then they should equal to each other,
+    *
+    *          {{{
+    *          structuralAPlusB should be(structuralXPlusY)
+    *          }}}
+    *
+    *          and they should have the same [[hashCode]].
+    *
+    *          {{{
+    *          structuralAPlusB.hashCode should be(structuralXPlusY.hashCode)
+    *          }}}
+    */
   protected trait TermApi extends super.TermApi { this: Term =>
     override def hashCode(): Int = {
       tree.structuralHashCode(new HashCodeContext)
